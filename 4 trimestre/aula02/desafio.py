8276eb6cb423b24b6de6705e182b48b343f86f15
BlunerS <71131510+BlunerS@users.noreply.github.com>
2023-03-01 20:40:29 -0300

    23.03.01 - 02

--- 4 trimestre/aula02/desafio.py
+++ 4 trimestre/aula02/desafio.py
@@ -1,10 +1,33 @@
-a = float(input('digite o 1 numero: '))
-b = float(input('digite o 2 numero: '))
-c = float(input('Digite o 3 numero: '))
+a = float(0)
+b = float(0)
+c = float(0)
 
-soma = float(a+b+c)
-media = float(soma / 3)
+def ler_numeros():
+    global a 
+    global b 
+    global c 
 
-print('Os numeros lidos são: ', (a),', ', (b), 'e', (c))
-print('A soma é:', soma)
-print('A média é:', media)
+    a = float(input('digite o 1 numero: '))
+    b = float(input('digite o 2 numero: '))
+    c = float(input('Digite o 3 numero: '))
+
+    print(f'Os numeros lidos são: ', (a),', ', (b), 'e', (c))
+
+ler_numeros()
+
+def calcular_soma():
+    global a
+    global b
+    global c
+
+    soma = float(a+b+c)
+    print(f'A soma dos números é:', soma)
+
+calcular_soma()
+
+def calcular_media():
+     
+     media = (a + b + c / 3)
+     print(f'A média desses números é: ',media)
+
+calcular_media()
